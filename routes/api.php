8162f938ed1c705689ea9d12c108c34e68e24920
王稔太 <wangrentai@wangrentaideMacBook-Pro.local>
2023-07-10 00:44:44 +0800

    feat::新增api insert posts, comments table功能路徑

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::prefix('insertData')->group(function () {
+    Route::post('posts', 'App\Http\Controllers\InsertController@posts');
+    Route::post('comments', 'App\Http\Controllers\InsertController@comments');
 });
