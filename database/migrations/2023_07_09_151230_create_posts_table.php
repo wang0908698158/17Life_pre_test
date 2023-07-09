212e157bb31b3d409476da84139803ec722c922b
王稔太 <wangrentai@wangrentaideMacBook-Pro.local>
2023-07-10 00:53:42 +0800

    feat:新增posts comments table migration

---
+++ database/migrations/2023_07_09_151230_create_posts_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id'); // 關聯comments的key
+            $table->string('title', 20); //標題
+            $table->string('content', 50); //內容
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
